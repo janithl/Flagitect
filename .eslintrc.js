96aedc9e50558bd8ad431a068ff700e63084b5bf
Janith Leanage <janithleanage@gmail.com>
2020-05-24 08:18:24 +0530

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    '@react-native-community',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+};
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default (): JSX.Element => (
+  <View>
+    <Text>Hello World!</Text>
+  </View>
+);
