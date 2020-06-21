a42b26d76d40f533dcfeeaa6105bd83262e5bd66
Janith Leanage <janithleanage@gmail.com>
2020-06-21 07:50:19 +0530

    refactored code and added tests

--- src/components/Palette.tsx
+++ src/components/Palette.tsx
@@ -5,33 +5,23 @@ import { ColourSwatch, Spinner, SpinnerTypes } from '@components';
 import Actions from '@lib/actions';
 import { ModalActions, openModal } from '@lib/reducers';
 import { ReducerAction } from '@lib/state';
+import { HSVtoRGB } from '@lib/utils';
 import { gnomePalette } from '@res/colours';
 
 enum PaletteTypes {
   Gnome = 'Gnome',
   Custom = 'Custom',
 }
+const PaletteTypeList = [PaletteTypes.Gnome, PaletteTypes.Custom];
 
-const k = (n, H) => (n + H / 60) % 6;
-const f = (n, H, S, V) =>
-  V - V * S * Math.max(0, Math.min(1, Math.min(k(n, H), 4 - k(n, H))));
-const comp = (rgb) =>
-  Math.round(255 * rgb)
-    .toString(16)
-    .padStart(2, '0')
-    .toUpperCase();
-const rgb = (H, S, V) =>
-  ['#', comp(f(5, H, S, V)), comp(f(3, H, S, V)), comp(f(1, H, S, V))].join('');
-const valIncrements = [0.28, 0.46, 0.64, 0.82, 1];
-const satIncrements = [1, 0.8, 0.6, 0.4, 0.2, 0];
-
+/** create palette based on hue selected */
 const getPalette = (hue: number) => {
   const palette: string[] = [];
-  satIncrements.forEach((s: number) => {
-    valIncrements.forEach((v: number) => {
-      palette.push(rgb(hue, s, v));
-    });
-  });
+  for (let sat = 1; sat >= 0; sat -= 0.2) {
+    for (let val = 0.28; val <= 1; val += 0.16) {
+      palette.push(HSVtoRGB(hue, sat, val));
+    }
+  }
   return palette;
 };
 
@@ -42,6 +32,7 @@ export default ({
 }: OwnProps): JSX.Element => {
   const [palette, setPalette] = useState(0);
   const [hue, setHue] = useState(180);
+  const paletteType = PaletteTypeList[palette];
 
   const onSelect = (payload: string) => {
     switch (selectAction) {
@@ -71,9 +62,34 @@ export default ({
     }
   };
 
+  const paletteSelector = () => (
+    <>
+      <Spinner
+        type={SpinnerTypes.List}
+        label={'Palette'}
+        list={PaletteTypeList}
+        value={palette}
+        setValue={setPalette}
+      />
+      {paletteType === PaletteTypes.Custom && (
+        <Spinner
+          type={SpinnerTypes.Number}
+          label={'Hue'}
+          value={hue}
+          setValue={(value: number) => setHue(value % 360)}
+          step={15}
+          min={0}
+          max={360}
+        />
+      )}
+    </>
+  );
+
   return (
     <FlatList
-      data={palette === 0 ? gnomePalette : getPalette(hue)}
+      data={
+        paletteType === PaletteTypes.Custom ? getPalette(hue) : gnomePalette
+      }
       renderItem={({ item }) => (
         <ColourSwatch colour={item} onPress={() => onSelect(item)} />
       )}
@@ -81,28 +97,7 @@ export default ({
       numColumns={5}
       columnWrapperStyle={styles.columnStyle}
       ListFooterComponent={<View style={styles.footer} />}
-      ListHeaderComponent={
-        <>
-          <Spinner
-            type={SpinnerTypes.List}
-            label={'Palette'}
-            list={[PaletteTypes.Gnome, PaletteTypes.Custom]}
-            value={palette}
-            setValue={setPalette}
-          />
-          {palette === 1 && (
-            <Spinner
-              type={SpinnerTypes.Number}
-              label={'Hue'}
-              value={hue}
-              setValue={(value: number) => setHue(value % 360)}
-              step={15}
-              min={0}
-              max={360}
-            />
-          )}
-        </>
-      }
+      ListHeaderComponent={paletteSelector}
     />
   );
 };
