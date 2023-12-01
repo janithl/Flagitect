15224ee190a0c1ddbf9a7f27465066c5a14ab391
Janith Leanage <janithleanage@gmail.com>
2023-12-02 12:19:54 +1300

    Add TextEncoder polyfill for Hermes

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
+// Polyfill since Hermes does not support the TextEncoder interface
+global.TextEncoder = require('text-encoding').TextEncoder;
+
 AppRegistry.registerComponent(appName, () => App);
