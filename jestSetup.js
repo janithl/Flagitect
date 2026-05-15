80e79352a63685bdf2d8a6686953802939f2ce27
Janith Leanage <janithleanage@gmail.com>
2026-05-16 04:24:27 +1200

    fix: unit test issue

--- jestSetup.js
+++ jestSetup.js
@@ -1,6 +1,6 @@
-import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
-
-jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest'),
+);
 
 jest.mock('react-native-blob-util', () => ({
   DocumentDir: jest.fn(),
